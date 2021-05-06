c56d1b8f5dd205ac646d04501332b7529ba0e04e
shades <justin@penguinzmedia.group>
2021-05-06 10:28:29 -0700

    Updating image, fixing files, etc.

--- upstart.sh
+++ upstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 --version
 pip3 install zipfile38 requests
-git clone --branch development https://github.com/Penguinz-Media-Group/rust_oxide_python_updater.git /opt/rust
+git clone --branch Development https://github.com/Penguinz-Media-Group/rust_oxide_python_updater.git /opt/rust
 ansible-playbook -i localhost  /opt/rust/buildrustserver.yml
 
 
